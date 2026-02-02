c5767bae5a555b94e0e4c95d075c093b13777360
Avadh Dhameliya <dhameliyaavadh592@gmail.com>
2026-02-02 18:07:35 +0530

    Prepared for Vercel Deployment with Cloudinary

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,11 +1,13 @@
+// admin/src/services/apiConfig.js
+
 const hostname = window.location.hostname;
 
 export const API_BASE_URL = 
   (hostname === "localhost" || hostname === "127.0.0.1")
-    ? "http://localhost:5000/api"  // Local Development
-    : "/api";                      // Vercel Production
+    ? "http://localhost:5000/api"  // Local: Connect to local backend
+    : "/api";                      // Vercel: Connect to relative backend
 
-export const STATIC_BASE_URL = 
+export const BASE_URL = 
   (hostname === "localhost" || hostname === "127.0.0.1")
-    ? "http://localhost:5000"      // Local Images
-    : "";                          // Vercel (Images load from Cloudinary)
+    ? "http://localhost:5000"
+    : "";
